c6c9839b46761237c345d2ec5bbda1bd5fc242ad
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 22:28:51 +0700

    Menampilkan icon Dropdown standar, ikon berubah sesuai status, dan memberi indikasi apakah Dropdown terbuka/tertutup

--- app/src/main/java/com/dwan/navdata_mvvm/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/widget/DynamicSelectedField.kt
@@ -2,6 +2,7 @@ package com.dwan.navdata_mvvm.ui.widget
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,7 +33,9 @@ fun DynamicSelectedTextField(
             value = selectedValue,
             onValueChange = {},
             label = { Text(text = label) },
-            trailingIcon = {}
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            }
         )
     }
 }
