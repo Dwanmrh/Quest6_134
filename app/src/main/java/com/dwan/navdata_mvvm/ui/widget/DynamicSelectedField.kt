07366e1c0a10f8e2afa046bc9aaeac1c8f60cc21
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 22:19:36 +0700

    Mendefinisikan fungsi DynamicSelectedField dengan parameter nilai terpilih saat ini, daftar pilihan, label untuk TextField, dan callback saat nilai berubah

--- app/src/main/java/com/dwan/navdata_mvvm/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/widget/DynamicSelectedField.kt
@@ -1,2 +1,15 @@
 package com.dwan.navdata_mvvm.ui.widget
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+}
