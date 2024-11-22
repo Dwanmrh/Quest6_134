5e92d53479b50f619b47a4f9533899b88aa7154d
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 20:26:40 +0700

    Membuat variable nim, nama, dan email dengan state yang bisa diperbarui. Dan membuat listData dari variable-variable tersebut

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
@@ -1,11 +1,19 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun MahasiswaFormView(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ) {
+    var nim by remember { mutableStateOf("") }
+    var nama by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
 
+    var listData:MutableList<String> = mutableListOf(nim, nama, email)
 }
