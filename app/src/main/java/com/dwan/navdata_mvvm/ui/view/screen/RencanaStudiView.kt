ab5d6109861e41c62a115aed78c4cec1d775e911
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 21:54:44 +0700

    Membuat state untuk dropdown dengan nilai awal string kosong. Membuat state boolean untuk checkbox dengan nilai awal adalah false. Membuat state untuk pilihan kelas dengan nilai awal string kosong. Membuat daftar mutable berisi chosenDropdown dan pilihanKelas dengan tipe data MutableList string

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
@@ -1,6 +1,10 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.dwan.navdata_mvvm.model.Mahasiswa
 
 @Composable
@@ -8,4 +12,10 @@ fun RencanaStudiView(
     mahasiswa: Mahasiswa,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
-) {}
+) {
+    var chosenDropdown by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember { mutableStateOf("") }
+
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+}
