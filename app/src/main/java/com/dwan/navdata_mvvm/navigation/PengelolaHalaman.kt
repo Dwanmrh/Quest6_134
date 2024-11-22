38c649edc9d7b375ec716f8e35cfb29702a08ad3
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 23:06:31 +0700

    Mendefinisikan daftar halaman (Splash, Mahasiswa, Matakuliah, Tampil) menggunakan enum dan membuat fungsi MahasiswaApp sebagai kerangka aplikasi dengan ViewModel dan pengontrol navigasi untuk mengelola tampilan dan perpindahan antarhalaman.

--- app/src/main/java/com/dwan/navdata_mvvm/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/navigation/PengelolaHalaman.kt
@@ -1,2 +1,26 @@
 package com.dwan.navdata_mvvm.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.dwan.navdata_mvvm.ui.view.viewmodel.MahasiswaViewModel
+import com.dwan.navdata_mvvm.ui.view.viewmodel.RencanaStudiViewModel
+
+enum class Halaman {
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudiViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+
+}
