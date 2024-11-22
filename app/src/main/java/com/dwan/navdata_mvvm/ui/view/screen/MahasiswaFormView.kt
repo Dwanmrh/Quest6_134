116f182a7f7f427c74f9a23fc72e22a4930291cf
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 20:30:39 +0700

    Membuat Column yang mengisi seluruh layar dengan latar belakang primary hijau gelap, dan rata tengah horizontal

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
@@ -1,10 +1,17 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.dwan.navdata_mvvm.R
 
 @Composable
 fun MahasiswaFormView(
@@ -16,4 +23,16 @@ fun MahasiswaFormView(
     var email by remember { mutableStateOf("") }
 
     var listData:MutableList<String> = mutableListOf(nim, nama, email)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 }
