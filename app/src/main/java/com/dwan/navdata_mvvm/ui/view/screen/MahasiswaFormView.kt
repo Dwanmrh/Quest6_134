22ce03a61cb721a592e16f9a5e7a0c228ca03f6e
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 20:22:09 +0700

    Membuat fungsi MahasiswaFormView dengan parameter fungsi onSubmitButtonClicked yang menerima List string dan mengembalikan nilai. Dan onBackButtonClicked

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
@@ -1,2 +1,11 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MahasiswaFormView(
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+) {
+
+}
