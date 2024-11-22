74abacd0df9bf38e09faab58f16aa2b9e6b3886f
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 23:04:10 +0700

    ViewModel yang mengelola data rencana studi mahasiswa menggunakan StateFlow, dengan fungsi untuk mengatur mata kuliah, kelas, dan menyimpan data rencana studi berdasarkan input yang diberikan

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/viewmodel/RencanaStudiViewModel.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/viewmodel/RencanaStudiViewModel.kt
@@ -1,4 +1,28 @@
 package com.dwan.navdata_mvvm.ui.view.viewmodel
 
-class RencanaStudiViewModel {
+import androidx.lifecycle.ViewModel
+import com.dwan.navdata_mvvm.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudiViewModel : ViewModel() {
+
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { state -> state.copy(namaMK = mkPilihan) }
+    }
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { state -> state.copy(kelas = kelasPilihan) }
+    }
+    fun saveDataKRS(data: MutableList<String>) {
+        _krsState.update { status -> status.copy(
+            namaMK = data[0],
+            kelas  = data[1]
+        )
+        }
+    }
 }
