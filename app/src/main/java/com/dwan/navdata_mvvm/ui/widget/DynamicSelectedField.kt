bf21a5776447c89c399c41cacfbf53386ac0d282
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 22:30:21 +0700

    Menyiapkan TextField dengan warna default dan mengatur agar komponen tersebut memiliki lebar penuh serta dapat digunakan sebagai anchor menu.

--- app/src/main/java/com/dwan/navdata_mvvm/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/widget/DynamicSelectedField.kt
@@ -1,9 +1,11 @@
 package com.dwan.navdata_mvvm.ui.widget
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -35,7 +37,11 @@ fun DynamicSelectedTextField(
             label = { Text(text = label) },
             trailingIcon = {
                 ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
-            }
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
         )
     }
 }
