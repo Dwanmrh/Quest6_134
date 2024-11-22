e874d28463fca46d890844ec6e9a3870e90fca78
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 22:10:15 +0700

    Membuat Box yang mengisi lebar penuh, mengatur latar belakang berwarna putih, dan membuat sudut atas kanan dan kiri melengkung

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Icon
@@ -85,5 +86,16 @@ fun RencanaStudiView(
                 )
             }
         }
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+                .fillMaxWidth()
+        )
     }
 }
