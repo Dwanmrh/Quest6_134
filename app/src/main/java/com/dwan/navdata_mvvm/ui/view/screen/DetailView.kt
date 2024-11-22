20effd954ebf56b8adb452a1ece798c9a68cc142
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 22:56:16 +0700

    Menampilkan teks "Detail Mahasiswa" dengan ukuran font 20sp dan gaya huruf tebal (bold)

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/DetailView.kt
@@ -1,6 +1,15 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.dwan.navdata_mvvm.model.Mahasiswa
 import com.dwan.navdata_mvvm.model.RencanaStudi
 
@@ -11,5 +20,13 @@ fun DetailView(
     onBackButtonClicked: () -> Unit,
     onResetButtonClicked: () -> Unit
 ) {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text("Detail Mahasiswa", fontSize = 20.sp, fontWeight = FontWeight.Bold)
 
+    }
 }
