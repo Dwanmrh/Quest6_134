ec87cb73915800569dc7b3f766140ec6d7b5b9ed
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 21:56:45 +0700

    Membuat Column lebar penuh berwarna primary hijau gelap

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
@@ -1,10 +1,16 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.dwan.navdata_mvvm.R
 import com.dwan.navdata_mvvm.model.Mahasiswa
 
 @Composable
@@ -18,4 +24,10 @@ fun RencanaStudiView(
     var pilihanKelas by remember { mutableStateOf("") }
 
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = colorResource(id = R.color.primary))
+    ) {  }
 }
