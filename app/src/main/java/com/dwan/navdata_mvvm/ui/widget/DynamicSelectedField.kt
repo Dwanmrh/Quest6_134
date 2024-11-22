ee1d615c14f21a904d8855b53d00b0f011df1767
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 22:25:34 +0700

    Membuat  TextField read-only untuk menampilkan pilihan Dropdown

--- app/src/main/java/com/dwan/navdata_mvvm/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/widget/DynamicSelectedField.kt
@@ -2,6 +2,8 @@ package com.dwan.navdata_mvvm.ui.widget
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -24,5 +26,13 @@ fun DynamicSelectedTextField(
         expanded = expanded,
         onExpandedChange = {expanded = !expanded},
         modifier = modifier
-    ) { }
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label) },
+            trailingIcon = {}
+        )
+    }
 }
