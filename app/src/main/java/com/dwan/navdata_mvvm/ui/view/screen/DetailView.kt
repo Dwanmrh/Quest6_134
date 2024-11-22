24e69887c1fb73279254550e5727902902dce52a
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 23:26:57 +0700

    Update Design

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/DetailView.kt
@@ -1,17 +1,34 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.dwan.navdata_mvvm.R
 import com.dwan.navdata_mvvm.model.Mahasiswa
 import com.dwan.navdata_mvvm.model.RencanaStudi
 
@@ -24,41 +41,86 @@ fun DetailView(
 ) {
     Column(
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+            .fillMaxWidth()
+            .background(color = colorResource(id = R.color.primary))
     ) {
-        Text("Detail Mahasiswa", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-
-        // Menampilkan data Mahasiswa
-        DetailRow(label = "Nama", value = mahasiswa.nama)
-        DetailRow(label = "NIM", value = mahasiswa.nim)
-        DetailRow(label = "Email", value = mahasiswa.email)
-
-        Spacer(modifier = Modifier.padding(8.dp))
-
-        // Menampilkan Rencana Studi
-        Text("Rencana Studi", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-        DetailRow(label = "Mata Kuliah", value = rencanaStudi.namaMK)
-        DetailRow(label = "Kelas", value = rencanaStudi.kelas)
-
-        Spacer(modifier = Modifier.padding(8.dp))
-
-        // Tombol Navigasi
-        Button(
-            onClick = onBackButtonClicked,
-            modifier = Modifier.fillMaxWidth()) {
-            Text("Kembali")
-        }
-        Button(
-            onClick = onResetButtonClicked,
-            modifier = Modifier.fillMaxWidth()
+        // Hheader
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text("Reset")
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {}
+            Box {
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+        }
+
+        //DetailView
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                // Header halaman
+                Text("Detail Mahasiswa", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+
+                // Data Mahasiswa
+                DetailRow(label = "Nama", value = mahasiswa.nama)
+                DetailRow(label = "NIM", value = mahasiswa.nim)
+                DetailRow(label = "Email", value = mahasiswa.email)
+
+                Spacer(modifier = Modifier.padding(8.dp))
+
+                // Data Rencana Studi
+                Text("Rencana Studi", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                DetailRow(label = "Mata Kuliah", value = rencanaStudi.namaMK)
+                DetailRow(label = "Kelas", value = rencanaStudi.kelas)
+
+                Spacer(modifier = Modifier.padding(16.dp))
+
+                // Tombol
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button(onClick = { onBackButtonClicked() }) {
+                        Text("Kembali")
+                    }
+                    Button(onClick = { onResetButtonClicked() }) {
+                        Text("Reset")
+                    }
+                }
+            }
         }
     }
 }
-
 @Composable
 fun DetailRow(label: String, value: String) {
     Column(modifier = Modifier.padding(bottom = 8.dp)) {
