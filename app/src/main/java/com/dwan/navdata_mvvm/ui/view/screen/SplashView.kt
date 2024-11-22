114782690729c8739778e34d8b39d8e3c8671f81
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 20:03:04 +0700

    Membuat fungsi Image untuk menampilkan logo umy dan mengatur size nya 150.dp

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/SplashView.kt
@@ -1,13 +1,17 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.dwan.navdata_mvvm.R
 
 @Composable
@@ -21,6 +25,9 @@ fun SplashView(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Image(painter = painterResource(
+            id = R.drawable.umy),
+            contentDescription = "", modifier = Modifier.size(150.dp)
+        )
     }
 }
