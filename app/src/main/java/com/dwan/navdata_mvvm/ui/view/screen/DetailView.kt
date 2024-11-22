fda5133a87f190b91ce2afd0ab24ca98cd9122b4
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 22:54:46 +0700

    Fungsi DetailView menampilkan detail mahasiswa dan rencana studi, serta menyediakan tombol untuk kembali dan mereset data

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/DetailView.kt
@@ -1,2 +1,15 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import com.dwan.navdata_mvvm.model.Mahasiswa
+import com.dwan.navdata_mvvm.model.RencanaStudi
+
+@Composable
+fun DetailView(
+    mahasiswa: Mahasiswa,
+    rencanaStudi: RencanaStudi,
+    onBackButtonClicked: () -> Unit,
+    onResetButtonClicked: () -> Unit
+) {
+
+}
