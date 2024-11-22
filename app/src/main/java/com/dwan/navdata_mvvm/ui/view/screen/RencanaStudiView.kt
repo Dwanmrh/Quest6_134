06f59525ed669859b447308d08ceffdb25ff09c8
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 21:49:27 +0700

    Mendefinisikan fungsi RencanaStudiView untuk menampilkan tampilan rencana studi mahasiswa, dengan parameter mahasiswa menerima object Mahasiswa yang akan ditampilkan datanya. Parameter onSubmitButtonCllicked dipanggil ketika button submit ditekan, menerima parameter mutable string. Parameter onBackButtonClicked dipanggil ketika tombol kembali ditekan

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
@@ -1,2 +1,11 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import com.dwan.navdata_mvvm.model.Mahasiswa
+
+@Composable
+fun RencanaStudiView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+) {}
