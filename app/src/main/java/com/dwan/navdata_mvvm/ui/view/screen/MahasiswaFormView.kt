c6f5e3f2f888793ed6047a144fe97869a7259a2c
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 20:35:35 +0700

    Membuat Spacer untuk memberi jarak vertikal, Row untuk menyusun widget secara horizontal dan rata tengah vertikal

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
@@ -2,7 +2,10 @@ package com.dwan.navdata_mvvm.ui.view.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import com.dwan.navdata_mvvm.R
 
 @Composable
@@ -33,6 +37,11 @@ fun MahasiswaFormView(
                 )
             ), horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
 
+        }
     }
 }
