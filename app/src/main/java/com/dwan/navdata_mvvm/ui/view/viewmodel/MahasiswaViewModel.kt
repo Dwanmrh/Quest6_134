f8905fe531034e9f180c580eb3b7a5daecc24f06
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 23:02:52 +0700

    ViewModel yang menyimpan data mahasiswa menggunakan StateFlow, memungkinkan data seperti NIM, nama, dan email diperbarui dengan fungsi saveDataMahaSiswa berdasarkan daftar nilai yang diberikan

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,27 @@
 package com.dwan.navdata_mvvm.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.dwan.navdata_mvvm.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+
+    // Request atau event
+    private val _statusUI = MutableStateFlow(Mahasiswa())
+
+    // Response atau state
+    val statusUI: StateFlow<Mahasiswa> = _statusUI.asStateFlow()
+
+    fun saveDataMahaSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nim = ls[0],
+                nama = ls[1],
+                email = ls[2]
+            )
+        }
+    }
 }
