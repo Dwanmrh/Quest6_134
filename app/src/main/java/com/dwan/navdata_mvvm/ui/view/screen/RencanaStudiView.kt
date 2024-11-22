cdc8832f16c9976ebc12736d5db36738d544e65e
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 22:40:22 +0700

    Membuat tata letak dengan spasi, garis pemisah, dan dua teks penjelasan.

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/RencanaStudiView.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -119,6 +120,15 @@ fun RencanaStudiView(
                     onValueChangedEvent = {
                         chosenDropdown = it
                     }
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(text = "Pilih kelas Belajar", fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Silahkan pilih kelas dari mata kuliah yang anda inginkan",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light
                 )
             }
         }
