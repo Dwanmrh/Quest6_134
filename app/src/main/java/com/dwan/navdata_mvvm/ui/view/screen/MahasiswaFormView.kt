af3574f2d23c36fd042a43a6faefbe6601ec7618
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 20:37:23 +0700

    Membuat Image untuk logo umy dengan ukuran 45.dp

--- app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/ui/view/screen/MahasiswaFormView.kt
@@ -1,11 +1,13 @@
 package com.dwan.navdata_mvvm.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +16,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.dwan.navdata_mvvm.R
 
@@ -41,7 +44,12 @@ fun MahasiswaFormView(
         Row(
             verticalAlignment = Alignment.CenterVertically
         ) {
-
+            Image(
+                painter = painterResource(
+                    id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier.size(45.dp)
+            )
         }
     }
 }
