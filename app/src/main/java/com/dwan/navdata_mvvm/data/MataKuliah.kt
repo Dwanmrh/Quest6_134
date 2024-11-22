0f8694e008bf04871ac3dfc105052c9f39066900
RidwanC134 <ridwanhh10@gmail.com>
2024-11-22 14:27:23 +0700

    Mendefinisikan object MataKuliah dengan property val options yang berisi list mata kuliah

--- app/src/main/java/com/dwan/navdata_mvvm/data/MataKuliah.kt
+++ app/src/main/java/com/dwan/navdata_mvvm/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.dwan.navdata_mvvm.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
 }
